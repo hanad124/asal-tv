47545555c997507030baa666e19cbeec51cefe9e
Hanad Mohamed <hanaddaahir124@gmail.com>
2024-04-23 13:51:35 +0300

    Added SEO.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,29 +1,50 @@
-import './globals.css'
-import './assets/css/tailwind.css'
-import './assets/css/materialdesignicons.min.css'
-import { Inter , Lexend } from 'next/font/google'
+import "./globals.css";
+import "./assets/css/tailwind.css";
+import "./assets/css/materialdesignicons.min.css";
+import { Inter, Lexend } from "next/font/google";
+import thum from "../../public/images/thum.png";
 
-const inter = Inter({ 
-  subsets: ['latin'],
-  weight: ['200','300','400','500','600', '700','800'],
-  variable: '--font-inter',
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-inter",
 });
 
-const lexend = Lexend({ 
-  subsets: ['latin'],
-  weight: ['200','300','400','500','600', '700','800'],
-  variable: '--font-lexend',
-})
+const lexend = Lexend({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-lexend",
+});
 
 export const metadata = {
-  title: 'Ovaxo - React Nextjs Multipurpose Landing Template',
-  description: 'Ovaxo - React Nextjs Multipurpose Landing Template',
-}
+  title: "Asal Tv - Watch Movies and TV Shows Exclusively",
+  description: "Watch Movies and TV Shows Exclusively on Asal Tv",
+
+  // Open Graph
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://asal-tv.vercel.app",
+    // site_name: "Tabaarak ICT Solutions",
+    images: [
+      {
+        url: thum.src,
+        width: 512,
+        height: 512,
+        alt: "Asal Tv - Watch Movies and TV Shows Exclusively",
+      },
+    ],
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" dir="LTR">
-      <body  className={`${inter.variable} ${lexend.variable} font-inter text-base text-slate-950 dark:text-white dark:bg-slate-900`}>{children}</body>
+      <body
+        className={`${inter.variable} ${lexend.variable} font-inter text-base text-slate-950 dark:text-white dark:bg-slate-900`}
+      >
+        {children}
+      </body>
     </html>
-  )
+  );
 }
